3a0fe5b20200793c2bdda5b8e1ada4e3b260443e
norovsuren15 <e.norvoo@gmail.com>
2026-02-27 10:16:06 +0800

    uurchlugdsun filea nemev'

--- movie-app/src/app/search/_components/SearchGenre.tsx
+++ movie-app/src/app/search/_components/SearchGenre.tsx
@@ -14,39 +14,44 @@ export const SearchGenre = async ({
   const { genres } = await getMovieByGenres();
 
   return (
-    <div className="flex flex-col px-5">
-      <p className="text-2xl mt-3 mb-8">Search filter</p>
-
-      <div className="max-w-97">
-        <div className="flex flex-col gap-1 mb-5">
-          <p>Search by genre</p>
-          <p>See lists of movies by genre</p>
-        </div>
-        <div className="flex flex-wrap gap-4 mt-5">
-          {genres.map((genre) => (
-            <Link href={`?genre=${genre.id}`} key={genre.id}>
-              <Badge key={genre.id} variant={"outline"} className="px-2.5">
-                {genre.name}
-              </Badge>
-            </Link>
-          ))}
+    <div className="flex flex-col px-5 md:px-15 lg:flex-row lg:gap-7">
+      <div>
+        <p className="text-2xl mt-3 mb-8">Search filter</p>
+        <div className="max-w-97 lg:w-96.75">
+          <div className="flex flex-col gap-1 mb-5">
+            <p>Search by genre</p>
+            <p>See lists of movies by genre</p>
+          </div>
+          <div className="flex flex-wrap gap-4 mt-5">
+            {genres.map((genre) => (
+              <Link href={`?genre=${genre.id}`} key={genre.id}>
+                <Badge key={genre.id} variant={"outline"} className="px-2.5">
+                  {genre.name}
+                </Badge>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
 
-      <p className="my-8">
-        {results.length} titles in {genre}
-      </p>
+      <div className="lg:border lg:bg-gray-100 lg:mt-20"></div>
 
-      <div className="grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-        {results.map((movie) => (
-          <Link href={`/${movie.id}`} key={movie.id}>
-            <MoviesListMovieCard
-              img={movie.poster_path}
-              rating={movie.vote_average}
-              movieName={movie.title}
-            />
-          </Link>
-        ))}
+      <div className="lg:mt-11">
+        <p className="my-8">
+          {results.length} titles in {genre}
+        </p>
+
+        <div className="grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {results.map((movie) => (
+            <Link href={`/${movie.id}`} key={movie.id}>
+              <MoviesListMovieCard
+                img={movie.poster_path}
+                rating={movie.vote_average}
+                movieName={movie.title}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
